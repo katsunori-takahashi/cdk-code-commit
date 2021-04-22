6ec07d3a10bca5b475670245ac04be8194e0b0e7
Katsunori Takahashi <katsunori_takahashi@pressman.ne.jp>
2021-04-22 13:55:05 +0900

    add create repository code

--- lib/cdk-code-commit-stack.ts
+++ lib/cdk-code-commit-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import {Repository} from '@aws-cdk/aws-codecommit';
+
+interface codeCommitContext {
+  "projectName": string
+}
 
 export class CdkCodeCommitStack extends cdk.Stack {
+
+  private readonly context: codeCommitContext;
+  private repository: Repository;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.context = this.node.tryGetContext(this.stackName);
+    this.repository = this.createRepository();
+  }
+
+  private createRepository() {
+    return new Repository(this, this.stackName + '-Repository', {
+      repositoryName: this.context.projectName,
+    });
   }
 }
